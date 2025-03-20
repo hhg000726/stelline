d1ff76a34f09b9ae1b76a88f5b0ec28787ab12bd
hhg000726 <ghdgusrl2000@gmail.com>
2025-03-20 22:46:11 +0900

    adjust delay

--- stelline/apis/search/search.py
+++ stelline/apis/search/search.py
@@ -19,7 +19,7 @@ def load_recent_data():
                 logging.info("SONGS_DATA_FILE 로드 완료")
         except (FileNotFoundError, json.JSONDecodeError):
             logging.error("SONGS_DATA_FILE 불러오기 실패")
-    if os.path.exists(RECENT_DATA_FILE):  # 파일이 존재하면 불러오고 3시간 대기 후 실행
+    if os.path.exists(RECENT_DATA_FILE):  # 파일이 존재하면 불러오고 대기 후 실행
         try:
             with open(RECENT_DATA_FILE, "r", encoding="utf-8") as f:
                 recent = json.load(f)
@@ -27,18 +27,23 @@ def load_recent_data():
         except (FileNotFoundError, json.JSONDecodeError):
             logging.error("RECENT_DATA_FILE이 불러오기 실패")
 
-        # 3시간 대기 후 API 시작
-        logging.info("RECENT_DATA_FILE이 있으므로, 첫 검색을 3시간 지연..")
-        threading.Thread(target=delayed_search_start, daemon=True).start()
+        searched_time = songs["searched_time"]
+        now = time.time()
+        delay = max(0, searched_time + 3 * 3600 - now)
+        formatted_searched_time = time.strftime("%H:%M:%S", time.localtime(searched_time))
+        formatted_delay = time.strftime("%H:%M:%S", time.localtime(delay))
+        # 대기 후 API 시작
+        logging.info(f"{formatted_searched_time}에 RECENT_DATA_FILE이 있으므로, 첫 검색을 {formatted_delay}만큼 지연..")
+        threading.Thread(target=delayed_search_start, daemon=True, args = (delay, )).start()
 
     else:  # 파일이 없으면 즉시 실행
         logging.info("RECENT_DATA_FILE이 없으므로 즉시 API 검색 시작")
         threading.Thread(target=search_api.search_api_process, daemon=True, args=(songs, recent)).start()
 
 # 딜레이 시작
-def delayed_search_start():
-    time.sleep(3 * 3600)  # 3시간 대기
-    logging.info("3시간 후 API 검색 시작")
+def delayed_search_start(delay):
+    time.sleep(delay)  # 대기
+    logging.info(f"{delay}초 후 API 검색 시작")
     search_api.search_api_process(songs, recent)
 
 # 초기 recent 데이터 로드

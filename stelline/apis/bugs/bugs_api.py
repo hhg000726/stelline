89a64703d8227be3b3d5a8e568dbce118aa3611d
hhg000726 <ghdgusrl2000@gmail.com>
2025-03-21 02:50:53 +0900

    logging for debug

--- stelline/apis/bugs/bugs_api.py
+++ stelline/apis/bugs/bugs_api.py
@@ -51,9 +51,12 @@ def bugs_api(name, url_number):
 
 # targets 로드
 def load_targets():
+    logging.info("hihi")
     if os.path.exists(TARGETS_FILE):
+        logging.info("hihihi")
         try:
             with open(TARGETS_FILE, "r", encoding="utf-8") as f:
+                logging.info("hihihihi")
                 return json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             logging.error("TARGETS_FILE 불러오기 실패")
@@ -63,6 +66,8 @@ def load_targets():
 def bugs_api_process(recent_data):
     while True:
         targets = load_targets()
+        logging.info("hi")
+        logging.info(targets)
         for target in targets:
             name = target["name"]
             title = target["title"]

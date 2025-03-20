621c82d53c1718b4b383d384e02cab2a6435f4ec
hhg000726 <ghdgusrl2000@gmail.com>
2025-03-21 02:32:16 +0900

    flow test

--- stelline/main.py
+++ stelline/main.py
@@ -3,5 +3,5 @@ from stelline import app
 from stelline.config import *
 
 if __name__ == "__main__":
-    logging.info("서버 시작됨!")
+    logging.info("서버 시작됨")
     app.run(host=HOST, port=PORT, debug=DEBUG_MODE, use_reloader=RELOADER_MODE)

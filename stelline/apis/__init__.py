ebd5bdfc00cafc549e4e60cd8a0b30b7bd2acad9
hhg000726 <ghdgusrl2000@gmail.com>
2025-03-21 02:24:25 +0900

    bp update 2

--- stelline/apis/__init__.py
+++ stelline/apis/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from stelline.apis.newOne import newOne_bp
 from stelline.apis.search import search_bp
+from stelline.apis.bugs import bugs_bp
+
 
 api_bp = Blueprint("api", __name__)
 
 # 각각의 앱 API 블루프린트 등록
 api_bp.register_blueprint(newOne_bp, url_prefix="/newOne")
 api_bp.register_blueprint(search_bp, url_prefix="/search")
-api_bp.register_blueprint(search_bp, url_prefix="/bugs")
+api_bp.register_blueprint(bugs_bp, url_prefix="/bugs")

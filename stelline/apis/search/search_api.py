82161b130e26c0964dc39e0ea5793c2e4a6bae88
hhg000726 <ghdgusrl2000@gmail.com>
2026-02-08 15:13:10 +0900

    대기 시간 조정

--- stelline/apis/search/search_api.py
+++ stelline/apis/search/search_api.py
@@ -201,12 +201,11 @@ def search_api():
 def search_api_process(by_admin=False):
     logging.info("주기적 검색 시작됨")
     while True:
-        if not by_admin:
-            time.sleep(SEARCH_API_INTERVAL - time.time() % (SEARCH_API_INTERVAL))
         try:
             new_songs = search_api()
             if not new_songs["all_songs"]:
                 logging.info("새로운 검색 데이터 없음")
+                time.sleep(SEARCH_API_INTERVAL - time.time() % (SEARCH_API_INTERVAL))
                 continue
             new_abnormal_songs = crawl_search_api()
             new_songs["all_songs"].extend(new_abnormal_songs["all_songs"])
@@ -248,3 +247,5 @@ def search_api_process(by_admin=False):
                 
         if by_admin:
             break
+        
+        time.sleep(SEARCH_API_INTERVAL - time.time() % (SEARCH_API_INTERVAL))
